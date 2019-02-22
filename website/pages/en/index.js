85a4483edebdb5e1a1b57965e8899b3ecbb43dac
Moein Alizadeh <moein.alizade@gmail.com>
2019-02-22 13:50:43 +0330

    Push for a test.

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -1,25 +1,7 @@
-/**
- * Copyright (c) 2017-present, Facebook, Inc.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
 const React = require('react');
-
-const CompLibrary = require('../../core/CompLibrary.js');
-
-const MarkdownBlock = CompLibrary.MarkdownBlock; /* Used to read markdown */
-const Container = CompLibrary.Container;
-const GridBlock = CompLibrary.GridBlock;
-
 class HomeSplash extends React.Component {
   render() {
-    const {siteConfig, language = ''} = this.props;
-    const {baseUrl, docsUrl} = siteConfig;
-    const docsPart = `${docsUrl ? `${docsUrl}/` : ''}`;
-    const langPart = `${language ? `${language}/` : ''}`;
-    const docUrl = doc => `${baseUrl}${docsPart}${langPart}${doc}`;
+  const {config} = this.props;
 
     const SplashContainer = props => (
       <div className="homeContainer">
@@ -27,19 +9,6 @@ class HomeSplash extends React.Component {
           <div className="wrapper homeWrapper">{props.children}</div>
         </div>
       </div>
-    );
-
-    const Logo = props => (
-      <div className="projectLogo">
-        <img src={props.img_src} alt="JSBooklet" />
-      </div>
-    );
-
-    const ProjectTitle = () => (
-      <h2 className="projectTitle">
-        {siteConfig.title}
-        <small>{siteConfig.tagline}</small>
-      </h2>
     );
 
     const PromoSection = props => (
@@ -60,13 +29,15 @@ class HomeSplash extends React.Component {
 
     return (
       <SplashContainer>
-        <Logo img_src={`${baseUrl}img/docusaurus.svg`} />
         <div className="inner">
-          <ProjectTitle siteConfig={siteConfig} />
+        <h2 className="projectTitle">
+          {config.title}
+          <small>{config.tagline}</small>
+        </h2>
           <PromoSection>
-            <Button href="#try">Try It Out</Button>
-            <Button href={docUrl('doc1.html')}>Example Link</Button>
-            <Button href={docUrl('doc2.html')}>Example Link 2</Button>
+            <Button href="#booklets"> 📒 کتابچه‌ها </Button>
+            <Button href="https://github.com/TrueMoein/JSBooklet/" target="_blank">گیت‌هاب</Button>
+            <Button href="/contribution"> 🤝 مشارکت </Button>
           </PromoSection>
         </div>
       </SplashContainer>
@@ -74,131 +45,24 @@ class HomeSplash extends React.Component {
   }
 }
 
+const Booklet = ({booklet}) => (
+  <a href={booklet.url}>
+    {booklet.name}
+  </a>
+)
+
 class Index extends React.Component {
   render() {
-    const {config: siteConfig, language = ''} = this.props;
-    const {baseUrl} = siteConfig;
-
-    const Block = props => (
-      <Container
-        padding={['bottom', 'top']}
-        id={props.id}
-        background={props.background}>
-        <GridBlock
-          align="center"
-          contents={props.children}
-          layout={props.layout}
-        />
-      </Container>
-    );
-
-    const FeatureCallout = () => (
-      <div
-        className="productShowcaseSection paddingBottom"
-        style={{textAlign: 'center'}}>
-        <h2>Feature Callout</h2>
-        <MarkdownBlock>These are features of this project</MarkdownBlock>
-      </div>
-    );
-
-    const TryOut = () => (
-      <Block id="try">
-        {[
-          {
-            content: 'Talk about trying this out',
-            image: `${baseUrl}img/docusaurus.svg`,
-            imageAlign: 'left',
-            title: 'Try it Out',
-          },
-        ]}
-      </Block>
-    );
-
-    const Description = () => (
-      <Block background="dark">
-        {[
-          {
-            content:
-              'This is another description of how this project is useful',
-            image: `${baseUrl}img/docusaurus.svg`,
-            imageAlign: 'right',
-            title: 'Description',
-          },
-        ]}
-      </Block>
-    );
-
-    const LearnHow = () => (
-      <Block background="light">
-        {[
-          {
-            content: 'Talk about learning how to use this',
-            image: `${baseUrl}img/docusaurus.svg`,
-            imageAlign: 'right',
-            title: 'Learn How',
-          },
-        ]}
-      </Block>
-    );
-
-    const Features = () => (
-      <Block layout="fourColumn">
-        {[
-          {
-            content: 'This is the content of my feature',
-            image: `${baseUrl}img/docusaurus.svg`,
-            imageAlign: 'top',
-            title: 'Feature One',
-          },
-          {
-            content: 'The content of my second feature',
-            image: `${baseUrl}img/docusaurus.svg`,
-            imageAlign: 'top',
-            title: 'Feature Two',
-          },
-        ]}
-      </Block>
-    );
-
-    const Showcase = () => {
-      if ((siteConfig.users || []).length === 0) {
-        return null;
-      }
-
-      const showcase = siteConfig.users
-        .filter(user => user.pinned)
-        .map(user => (
-          <a href={user.infoLink} key={user.infoLink}>
-            <img src={user.image} alt={user.caption} title={user.caption} />
-          </a>
-        ));
-
-      const pageUrl = page => baseUrl + (language ? `${language}/` : '') + page;
-
-      return (
-        <div className="productShowcaseSection paddingBottom">
-          <h2>Who is Using This?</h2>
-          <p>This project is used by all these people</p>
-          <div className="logos">{showcase}</div>
-          <div className="more-users">
-            <a className="button" href={pageUrl('users.html')}>
-              More {siteConfig.title} Users
-            </a>
-          </div>
-        </div>
-      );
-    };
+    const {config} = this.props;
+    const {booklets} = config;
 
     return (
       <div>
-        <HomeSplash siteConfig={siteConfig} language={language} />
-        <div className="mainContainer">
-          <Features />
-          <FeatureCallout />
-          <LearnHow />
-          {/*<TryOut />*/}
-          {/*<Description />*/}
-          {/*<Showcase />*/}
+        <HomeSplash config={config}/>
+        <div className="mainContainer" id="booklets">
+          {booklets.map(b => (
+            <Booklet key={b.name} booklet={b} />
+          ))}
         </div>
       </div>
     );
--- website/siteConfig.js
+++ website/siteConfig.js
@@ -1,47 +1,47 @@
-/**
- * Copyright (c) 2017-present, Facebook, Inc.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
-// See https://docusaurus.io/docs/site-config for all the possible
-// site configuration options.
-
 const siteConfig = {
-  title: 'کتابچه جاوااسکریپت', // Title for your website.
-  tagline: 'مقالات و آموزش های فارسی JavaScript',
-  url: 'https://JSBooklet.ir', // Your website URL
-  baseUrl: '/', // Base URL for your project */
-  // For github.io type URLs, you would set the url and baseUrl like:
-  //   url: 'https://facebook.github.io',
-  //   baseUrl: '/test-site/',
+  title: 'کتابچه جاوااسکریپت',
+  tagline: 'آموزش‌های فارسی جاوااسکریپت و محصولات مرتبط',
+  url: 'https://JSBooklet.ir',
+  baseUrl: '/',
 
-  // Used for publishing and more
   projectName: 'JSBooklet',
   organizationName: 'IranJS.com',
-  // For top-level user or org sites, the organization is still the same.
-  // e.g., for the https://JoelMarcey.github.io site, it would be set like...
-  //   organizationName: 'JoelMarcey'
-  // algolia: {
-  //   apiKey: 'my-api-key',
-  //   indexName: 'my-index-name',
-  //   algoliaOptions: {} // Optional, if provided by Algolia
-  // },
-  // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
     // {search: true},
-    {page: 'help', label: 'مشارکت'},
+    {page: 'contributin', label: 'مشارکت'},
     {blog: true, label: 'وبلاگ'},
-    {doc: 'javascript-introduction', label: 'کتابچه'},
+    {href: '/#booklets', label: 'کتابچه‌ها'},
   ],
 
-  /* path to images for header/footer */
+  docsUrl: "booklets",
+
+  booklets: [
+    {
+      name: "JavaScript",
+      image: "img/booklets/javascript.png",
+      url: "/booklets/javascript-introduction"
+    },
+    {
+      name: "TypeScript",
+      image: "img/booklets/typescript.png",
+      url: "/booklets/javascript-introduction"
+    },
+    {
+      name: "React",
+      image: "img/booklets/react.png",
+      url: "/booklets/javascript-introduction"
+    },
+    {
+      name: "Vue.js",
+      image: "img/booklets/vue.png",
+      url: "/booklets/javascript-introduction"
+    },
+  ],
+
   headerIcon: 'img/jsbooklet.png',
   footerIcon: 'img/jsbooklet.png',
   favicon: 'img/jsbooklet.png',
 
-  /* Colors for website */
   colors: {
     primaryColor: '#323330',
     secondaryColor: '#f7b92a',
@@ -50,34 +50,15 @@ const siteConfig = {
   docsSideNavCollapsible: true,
   editUrl: 'https://github.com/TrueMoein/JSBooklet/tree/master/docs/',
 
-  /* Custom fonts for website */
-  /*
-  fonts: {
-    myFont: [
-      "Times New Roman",
-      "Serif"
-    ],
-    myOtherFont: [
-      "-apple-system",
-      "system-ui"
-    ]
-  },
-  */
-
-  // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
   copyright: `Copyright © ${new Date().getFullYear()} IranJS.com`,
 
   highlight: {
-    // Highlight.js theme to use for syntax highlighting in code blocks.
     theme: 'atom-one-dark',
   },
 
-  // Add custom scripts here that would be placed in <script> tags.
   scripts: ['https://buttons.github.io/buttons.js'],
 
-  // On page navigation for the current documentation page.
   onPageNav: 'separate',
-  // No .html extensions for paths.
   cleanUrl: true,
 
   gaTrackingId: "UA-134867882-1",
@@ -87,18 +68,13 @@ const siteConfig = {
     zIndex: 100,
   },
 
-  // Open Graph and Twitter card images.
   ogImage: 'img/docusaurus.png',
   twitterImage: 'img/docusaurus.png',
 
-  // Show documentation's last contributor's name.
   enableUpdateBy: true,
 
-  // Show documentation's last update time.
   enableUpdateTime: true,
 
-  // You may provide arbitrary config keys to be used as needed by your
-  // template. For example, if you need your repo's URL...
     repoUrl: 'https://github.com/TrueMoein/JSBooklet',
 };
 
